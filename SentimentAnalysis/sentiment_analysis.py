429948e7b447318ff153bee18e4df59d96482ff0
chandramohan0 <thechandramohan01@gmail.com>
2023-09-29 15:48:24 -0400

    Code Quality improved of sentiment_analysis.py from 6.0/10 to 10.0/10

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,11 +1,29 @@
-import requests
+"""
+Sentiment Analysis Module
+
+This module provides functions for performing sentiment analysis on text data.
+"""
+
 import json
+import requests
 
 def sentiment_analyzer(text_to_analyse):
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    """
+    Analyze the sentiment of a given text.
+
+    Args:
+        text_to_analyse (str): The text to be analyzed.
+
+    Returns:
+        dict: A dictionary containing the sentiment label and score.
+    """
+    url = (
+    'https://sn-watson-sentiment-bert.labs.skills.network/v1/'
+    'watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    )
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json=myobj, headers=header, timeout=10)
     formatted_response = json.loads(response.text)
     if response.status_code == 200:
         label = formatted_response['documentSentiment']['label']
